f4a555358b32f3cd973a934a428f849bd912a361
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-02-21 20:27:50 +0700

    current time

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 let currentMusic = 0;
 
 const music = document.querySelector("#audio");
-const seekBar = document.querySelector(".music-name");
+const seekBar = document.querySelector(".seek-bar");
+const songName = document.querySelector(".music-name");
+// const songName = document.getElementsByClassName("music-name")
 const artistName = document.querySelector(".artist-name");
 const disk = document.querySelector(".disk");
 const currentTime = document.querySelector(".current-time");
@@ -12,7 +14,39 @@ const backwardBtn = document.querySelector(".backward-btn");
 
 playBtn.addEventListener("click", () => {
   playBtn.classList.toggle("pause");
-  disk.classList.toggle("play")
+  disk.classList.toggle("play");
 });
 
 // setup music
+const setMusic = (i) => {
+  seekBar.value = 0; // set range slide value to 0
+  let song = songs[i];
+  currentMusic = i;
+  music.src = song.path;
+
+  songName.innerHTML = song.name;
+  artistName.innerHTML = song.artist;
+  disk.style.backgroundImage = `url('${song.cover}')`;
+
+  currentTime.innerHTML = "00:00";
+  setTimeout(() => {
+    seekBar.max = music.duration;
+    console.log(music.duration);
+    musicDuration.innerHTML = formatTime(music.duration);
+  }, 300);
+};
+
+setMusic(1);
+
+// mendapatkan waktu format
+const formatTime = (time) => {
+  let min = Math.floor(time / 60);
+  if (min < 10) {
+    min = `0${min}`;
+  }
+  let sec = Math.floor(time % 60);
+  if (sec < 10) {
+    sec = `0${sec}`;
+  }
+  return `${min} : ${sec}`;
+};
--- js/data.js
+++ js/data.js
@@ -1,8 +1,14 @@
 let songs = [
    {
       name: "Kimetsu no Yaiba",
-      path: "../assets/musics/Gurenge.mp3",
+      path: "assets/musics/Gurenge.mp3",
       artist: "LISA",
-      cover: "images/cover1.png"
-   }
+      cover: "assets/images/Gurenge_Cover.webp"
+   },
+   {
+      name: "kangen",
+      path: "assets/musics/Gurenge.mp3",
+      artist: "FAQIH",
+      cover: "assets/images/cover 1.png"
+   },
 ]
